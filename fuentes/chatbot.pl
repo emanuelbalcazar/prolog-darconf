8b2f7b7eac874780b6ca015761a6c3d7785abe46
EXC30933 <emanuel.balcazar1@claro.com.ar>
2023-05-17 14:18:52 -0300

    Prueba chat

---
+++ fuentes/chatbot.pl
@@ -0,0 +1,24 @@
+/* 
+    Implementación de un chatbot super simple
+    Uso: 
+        jarvis([me, llamo, Emanuel], Respuesta).
+        jarvis([me, siento, cansado], Respuesta).
+ */
+
+jarvis(Stimuli, Response) :-
+    template(InternalStimuli, InternalResponse),
+    match(InternalStimuli, Stimuli),
+    match(InternalResponse, Response),
+    !.
+
+template([s([me, llamo]),s(X)], [s([como, estas]),s(X),w('?')]).
+
+match([],[]).
+match([Item|Items],[Word|Words]) :-
+    match(Item, Items, Word, Words).
+
+match(w(Word), Items, Word, Words) :-
+    match(Items, Words).
+match(s([Word|Seg]), Items, Word, Words0) :-
+    append(Seg, Words1, Words0),
+    match(Items, Words1).
