4a17e7cfc12a11197118c7c8ee556cce1bcb738e
EXC30933 <emanuel.balcazar1@claro.com.ar>
2023-05-17 14:19:51 -0300

    Prueba bot

--- fuentes/chatbot.pl
+++ fuentes/chatbot.pl
@@ -11,7 +11,11 @@ jarvis(Stimuli, Response) :-
     match(InternalResponse, Response),
     !.
 
+/* defino las preguntas y respuestas */
+
 template([s([me, llamo]),s(X)], [s([como, estas]),s(X),w('?')]).
+
+/* logica de matcheo de preguntas/respuestas */
 
 match([],[]).
 match([Item|Items],[Word|Words]) :-
@@ -19,6 +23,7 @@ match([Item|Items],[Word|Words]) :-
 
 match(w(Word), Items, Word, Words) :-
     match(Items, Words).
+
 match(s([Word|Seg]), Items, Word, Words0) :-
     append(Seg, Words1, Words0),
     match(Items, Words1).
